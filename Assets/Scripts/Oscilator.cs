6bdf9c184b548eddc302304b392c77ebe895bd33
NeatSquid <neatsquid@gmail.com>
2022-06-26 07:04:47 +0300

    oscilation start

--- Assets/Scripts/Oscilator.cs
+++ Assets/Scripts/Oscilator.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Oscilator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")] [Space] [SerializeField]
+    private Vector3 _direction;
+
+    [SerializeField] [Range(0f, 10f)] private float _amount;
+    [SerializeField] [Range(0f, 1f)] private float _factor;
+    private Vector3 _startPos;
+
+    private void Start()
     {
-        
+        _startPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var tran = transform;
+        var offset = _direction * (_amount * _factor);
+        var v = tran.right * offset.x + tran.up * offset.y + tran.forward * offset.z;
         
+        tran.position = _startPos + v;
     }
 }
